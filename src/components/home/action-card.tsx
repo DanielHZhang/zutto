3a9d0d30f0c15a935f01ee795fbc8a9335b94a24
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2022-04-03 12:08:43 -0400

    feat: add icons to action card

--- src/components/home/action-card.tsx
+++ src/components/home/action-card.tsx
@@ -1,3 +1,5 @@
+import OpenIcon from 'iconoir/icons/open-in-window.svg';
+import PlusIcon from 'iconoir/icons/plus.svg';
 import {JSX, JSXElement, splitProps} from 'solid-js';
 
 type Props = JSX.HTMLAttributes<HTMLDivElement> & {
@@ -9,18 +11,20 @@ export const ActionCard = (props: Props): JSXElement => {
   const [ownProps, htmlProps] = splitProps(props, ['title', 'description']);
   return (
     <div
-      class='flex justify-between bg-slate-700 hover:bg-slate-600 transition-colors rounded-lg p-4 max-w-xs cursor-pointer'
+      class='flex justify-between bg-slate-700 hover:bg-slate-600 transition-colors shadow-md rounded-lg p-4 max-w-xs cursor-pointer'
       {...htmlProps}
     >
-      <div class='flex'>
-        <div class='w-10 h-10' />
+      <div class='flex space-x-4'>
+        <div class='flex justify-center items-center'>
+          <OpenIcon />
+        </div>
         <div class='flex flex-col'>
           <h1 class='font-medium'>{ownProps.title}</h1>
           <div class='text-slate-400'>{ownProps.description}</div>
         </div>
       </div>
-      <div>
-        <span>+</span>
+      <div class='flex justify-center items-center'>
+        <PlusIcon width='32' height='32' />
       </div>
     </div>
   );
