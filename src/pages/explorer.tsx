88910cf23b5ee48b09f1fde65195e19d38bf7192
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2022-04-10 01:44:58 -0400

    feat: add sidebar column to explorer view

--- src/pages/explorer.tsx
+++ src/pages/explorer.tsx
@@ -1,7 +1,7 @@
 import {useParams} from 'solid-app-router';
 import type {JSXElement} from 'solid-js';
-import {createResource, createSignal} from 'solid-js';
-import {queryTableData} from 'src/actions';
+import {createResource, createSignal, For, Show} from 'solid-js';
+import {queryAllTables, queryTableData} from 'src/actions';
 import {Button} from 'src/components/base';
 import {Table, Tabs} from 'src/components/explorer';
 
@@ -26,23 +26,32 @@ export default function Explorer(): JSXElement {
   const [tableName, setTableName] = createSignal(params.tableName);
   const [tableData] = createResource(tableName, queryTableData);
 
+  const [tables] = createResource(queryAllTables);
+
   console.log('table data:', tableData());
   return (
-    <div class='flex flex-col'>
-      <div class='flex'>
-        <div>Home</div>
-        <Tabs />
+    <div class='flex'>
+      <div class='flex flex-col basis-80 min-w-50'>
+        <Show when={tables()} fallback={<div>Loading...</div>}>
+          <For each={tables()}>{(item) => <div>{item.name}</div>}</For>
+        </Show>
       </div>
-      <section class='flex'>
-        <Button>Refresh</Button>
-        <div>Filters button</div>
-        <div>Show number of records button</div>
-        <Button>Add Record</Button>
-        <Button>Save Changes</Button>
-      </section>
-      <main>
-        <Table data={data} headers={headers} />
-      </main>
+      <div class='flex flex-col'>
+        <div class='flex'>
+          <div>Home</div>
+          <Tabs />
+        </div>
+        <section class='flex'>
+          <Button>Refresh</Button>
+          <div>Filters button</div>
+          <div>Show number of records button</div>
+          <Button>Add Record</Button>
+          <Button>Save Changes</Button>
+        </section>
+        <main>
+          <Table data={data} headers={headers} />
+        </main>
+      </div>
     </div>
   );
 }
