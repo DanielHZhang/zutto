85ac26f692b787089d3d423911791dacc1d0341c
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2022-04-09 22:30:39 -0400

    feat: add modal content for renaming table, and autofocus input field on initial render

--- src/pages/tables.tsx
+++ src/pages/tables.tsx
@@ -1,7 +1,9 @@
 import Fuse from 'fuse.js';
+import NavArrowRightIcon from 'iconoir/icons/nav-arrow-right.svg';
 import PlusIcon from 'iconoir/icons/plus.svg';
 import type {JSXElement} from 'solid-js';
-import {createResource, createSignal, ErrorBoundary, Match, Show, Switch} from 'solid-js';
+import {createRenderEffect, createResource, ErrorBoundary, Match, Show, Switch} from 'solid-js';
+import {createStore} from 'solid-js/store';
 import {queryAllTables} from 'src/actions';
 import {Button, Grid, Heading, Input, Modal} from 'src/components/base';
 import {TableCard} from 'src/components/cards';
@@ -10,8 +12,12 @@ const NEW_TABLE_MODAL = 'New Table';
 const RENAME_TABLE_MODAL = 'Rename Table';
 
 export const Tables = (): JSXElement => {
-  const [modalOpen, setModalOpen] = createSignal<string | null>(null);
-  const [searchFilter, setSearchFilter] = createSignal('');
+  const [state, setState] = createStore({
+    modalOpen: '',
+    searchFilter: '',
+    selectedTable: '',
+    newTableName: '',
+  });
   const [tables] = createResource(queryAllTables);
 
   const filterTables = () => {
@@ -19,20 +25,26 @@ export const Tables = (): JSXElement => {
     if (!tableNames) {
       return [];
     }
-    if (!searchFilter()) {
+    if (!state.searchFilter) {
       return tableNames;
     }
     const fuse = new Fuse(tableNames, {includeScore: false, keys: ['name']});
-    const searchResults = fuse.search(searchFilter());
+    const searchResults = fuse.search(state.searchFilter);
     return searchResults.map((result) => result.item);
   };
 
   const onTableCardAction = (name: string) => (key: string) => {
     if (key === 'rename') {
-      setModalOpen(RENAME_TABLE_MODAL);
+      setState({modalOpen: RENAME_TABLE_MODAL, selectedTable: name});
     }
-    console.log('got table card action:', key);
   };
+
+  let renameInput: HTMLInputElement;
+  createRenderEffect(() => {
+    if (state.modalOpen === RENAME_TABLE_MODAL && renameInput) {
+      renameInput.focus();
+    }
+  });
 
   return (
     <section class='flex flex-grow-1 flex-col space-y-10 text-gray-200 p-8'>
@@ -40,14 +52,14 @@ export const Tables = (): JSXElement => {
       <ErrorBoundary fallback={(error) => <div>Error fetching tables: {error.message}</div>}>
         <Show when={tables()} fallback={<div>Loading...</div>}>
           <div class='flex space-x-4'>
-            <Button variant='primary' onClick={() => setModalOpen(NEW_TABLE_MODAL)}>
+            <Button variant='primary' onClick={() => setState('modalOpen', NEW_TABLE_MODAL)}>
               <PlusIcon />
               <span class='mx-1'>New Table</span>
             </Button>
             <Input
               class='flex-grow'
               placeholder='Search for table...'
-              onInput={(event) => setSearchFilter(event.currentTarget.value)}
+              onInput={(event) => setState('searchFilter', event.currentTarget.value)}
             />
           </div>
           <Grid items={filterTables()}>
@@ -55,15 +67,39 @@ export const Tables = (): JSXElement => {
           </Grid>
         </Show>
       </ErrorBoundary>
-      <Modal isOpen={modalOpen() !== null} onClose={() => setModalOpen(null)}>
+      <Modal isOpen={!!state.modalOpen} trapFocus={false} onClose={() => setState('modalOpen', '')}>
         <Switch>
-          <Match when={modalOpen() === NEW_TABLE_MODAL}>
-            <div>New Table</div>
-          </Match>
-          <Match when={modalOpen() === RENAME_TABLE_MODAL}>
-            <Heading>Rename Table</Heading>
-            <div class='flex'>
+          <Match when={state.modalOpen === NEW_TABLE_MODAL}>
+            <Heading class='mb-6'>New Table</Heading>
+            <div class='flex flex-col space-y-4'>
               <Input placeholder='New Table Name' />
+              <div class='flex justify-end space-x-4'>
+                <Button variant='ghost'>Cancel</Button>
+                <Button variant='primary'>
+                  <span class='ml-2'>Continue</span>
+                  <NavArrowRightIcon />
+                </Button>
+              </div>
+            </div>
+          </Match>
+          <Match when={state.modalOpen === RENAME_TABLE_MODAL}>
+            <Heading class='mb-6'>Rename Table</Heading>
+            <div class='flex flex-col space-y-4'>
+              <Input
+                ref={(element) => (renameInput = element)}
+                value={state.selectedTable}
+                placeholder='Table Name'
+                onInput={(event) => setState('newTableName', event.currentTarget.value)}
+              />
+              <div class='flex justify-end space-x-4'>
+                <Button variant='ghost' onClick={() => setState('modalOpen', '')}>
+                  Cancel
+                </Button>
+                <Button variant='primary'>
+                  <span class='ml-2'>Continue</span>
+                  <NavArrowRightIcon />
+                </Button>
+              </div>
             </div>
           </Match>
         </Switch>
